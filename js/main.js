63c741a8d42e1e931031d5ee999387a2fe548f1a
Yash Kothari <yashkothari1729@gmail.com>
2017-06-21 21:38:43 +0530

    Camera movement using angle

--- js/main.js
+++ js/main.js
@@ -11,21 +11,27 @@ if (Detector.webgl) {
 }
 */
 // Basic Setup
+var camRadius =200;
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
-camera.position.set( 0 , 0,200 );
+camera.position.set( 0 , 100,camRadius );
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 var renderer = new THREE.WebGLRenderer({alpha:true});
 renderer.autoClear = false;
 renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setClearColor( 0xffffff, 0);
+//renderer.setClearColor( 0x000000, 1);
+scene.background = new THREE.Color(0,0,0);
 document.body.appendChild( renderer.domElement );
 var geometry1 = new THREE.CubeGeometry(200, 200, 200);
 var material1 = new THREE.MeshBasicMaterial({
     color: 0xff0000,
     wireframe: true
 });
-
+var light = new THREE.PointLight( 0xff0000, 10 );
+light.position.set( 0,100, 0 );
+scene.add( light );
+var light2 = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light2 );
 mesh = new THREE.Mesh(geometry1, material1);
 scene.add(mesh);
 // Keyboard controls
@@ -33,7 +39,7 @@ var keyboard = new THREEx.KeyboardState();
 function createWall()
 {
     var geometryWall = new THREE.BoxGeometry( 10, 220, 50 );
-    var materialWall = new THREE.MeshBasicMaterial( {color: 0x00f220,opacity:0.6,transparent:true} );
+    var materialWall = new THREE.MeshBasicMaterial( {color: 0x00f0ff,opacity:0.6,transparent:true} );
     var cube1 = new THREE.Mesh( geometryWall, materialWall );
     var cube2 = new THREE.Mesh( geometryWall, materialWall );
     var cube3 = new THREE.Mesh( geometryWall, materialWall );
@@ -50,28 +56,37 @@ function createWall()
     scene.add( cube4 );
 }
 //OrbitControls
-controls = new THREE.OrbitControls( camera, renderer.domElement );
-controls.addEventListener( 'change', render ); // remove when using animation loop
+//controls = new THREE.OrbitControls( camera, renderer.domElement );
+//controls.addEventListener( 'change', render ); // remove when using animation loop
 // enable animation loop when using damping or autorotation
 //controls.enableDamping = true;
 //controls.dampingFactor = 0.25;
-controls.enableZoom = false;
+//controls.enableZoom = false;
 createWall();
 var size = 200;
 var divisions = 20;
 function createGrid(scene){
-    var grid = new THREE.GridHelper( size, divisions, 0x000fff,0x234876 );
+    var grid = new THREE.GridHelper( size, divisions, 0x1711b2,0x234876 );
     grid.position.set(0,0,0);
     scene.add( grid );
 }
-var snake = new Snake(scene);
+var snake = new Snake(scene,1000);
 
 createGrid(scene);
 var axisHelper = new THREE.AxisHelper( 500 );
 scene.add( axisHelper );
 renderer.render(scene,camera);
+/*
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
 window.addEventListener( 'resize', onWindowResize, false );
 renderer.clearDepth(); // important! clear the depth buffer
+
+*/
+var cameraTheta = 0,incUnit=0.2;
 function keyboardInput() {
     var change =false;
     if(keyboard.pressed("w"))
@@ -86,27 +101,86 @@ function keyboardInput() {
     if(keyboard.pressed("a"))
         if(snake.direction != 2|| snake.direction != 4 )
         {   snake.direction = 4;change = true;}
-    if(keyboard.pressed("up"))
+    if(keyboard.pressed("q"))
         if(snake.direction != 6|| snake.direction != 6 )
         {   snake.direction = 5;change = true;}
-    if(keyboard.pressed("down"))
+    if(keyboard.pressed("e"))
         if(snake.direction != 5|| snake.direction != 6 )
-        {   snake.direction =6;change = true;}
-   // return change;
-}
-function render(){
+            snake.direction =6;
+    if(keyboard.pressed("right"))
+        cameraTheta+=incUnit;
+    if(keyboard.pressed("left"))
+        cameraTheta-=incUnit;
 
+    camera.position.x = camRadius*Math.sin(cameraTheta);
+
+    camera.position.z = camRadius*Math.cos(cameraTheta);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+}
+function gameOver(){
+    clearInterval(animation);
+    while(scene.children.length > 0){
+        scene.remove(scene.children[0]);
+    }
+ //   scene.add()
+}
+function getEmptyBox() {
+
+    var position ={x:10*Math.floor(Math.random()*18-9)+5,y:10*Math.floor(Math.random()*18-9)+5,z:10*Math.floor(Math.random()*18-9)+5};
+    snake.body.forEach(function (part) {
+        if(part.position === position)
+            return getEmptyBox();
+    });
+    return position;
+}
+function Food(scene,id){
+    this.position={x:0,y:0};
+    this.id = id ;
+    this.scene = scene;
+    this.geometry = new THREE.SphereGeometry(5,32,32);
+    this.material = new THREE.MeshLambertMaterial({color:0xe21212});
+}
+Food.prototype = {
+    remove: function () {
+        this.scene.remove(scene.getObjectByName(this.id));
+    },
+    add: function () {
+        var temp = new THREE.Mesh(this.geometry,this.material);
+        temp.name = this.id;
+        this.position = getEmptyBox();
+        temp.position.set(this.position.x,this.position.y,this.position.z);
+        this.scene.add(temp);
+        console.log("hello",this.position);
+    }
+}
+function isFoodCollision() {
+    if(snake.body[0].position === food.position)
+    {
+        food.remove();
+        food.add();
+        return true;
+    }
+    else
+        return false;
+}
+var food = new Food(scene,"food");
+food.add();
+var t=0;
+function render(){
+        if(isFoodCollision())
+            snake.add();
+        if(snake.isSelfCrash())
+            gameOver();
         keyboardInput();
-        snake.move();
+        if(t%5 == 0)
+            if(!snake.move())
+            gameOver();
 	    renderer.render( scene, camera );
+	    t++;
 }
 snake.init();
-var fps = 7;
-setInterval( render, 1000/fps  );
+var fps = 30;
+var animation = setInterval( render, 1000/fps  );
 
 
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-}
--- js/snake.js
+++ js/snake.js
@@ -1,10 +1,12 @@
-function Snake(scene){
+function Snake(scene,boxsize){
     this.body = [];
     this.length = 2;
     this.scene = scene;
     this.direction = 5 ;
     this.geometry = new THREE.BoxGeometry(10,10,10);
-    this.material = new THREE.MeshLambertMaterial( { color: 0x00ff25 } );
+    this.material = new THREE.MeshLambertMaterial();
+    this.material.color = new THREE.Color(0,0,255)
+    this.boxsize = boxsize;
 }
 Snake.prototype = {
     init: function () {
@@ -12,7 +14,7 @@ Snake.prototype = {
         var y = -10 * Math.floor(Math.random() * 6);
         var z = 10 * Math.floor(Math.random() * 18 - 9);
 
-        for (var i = 0; i < 2; i++) {
+        for (var i = 0; i < 15; i++) {
             var temp = new THREE.Mesh(this.geometry, this.material);
             temp.position.set(x + 5,  y+5, z+5);
             y -= 10;
@@ -58,6 +60,40 @@ Snake.prototype = {
                 break;
         }
         this.body.unshift(temp);
+        if(this.isWallImpact())
+            return false;
+        else
+            return true;
+    },
+    isWallImpact: function(){
+        console.log(this.body[0].position);
+        if(this.body[0].position.x >= this.boxsize ||this.body[0].position.x <= -this.boxsize )
+            return true;
+        if(this.body[0].position.y >= this.boxsize ||this.body[0].position.y <= -this.boxsize )
+            return true;
+        if(this.body[0].position.z >= this.boxsize ||this.body[0].position.z <= -this.boxsize )
+            return true;
+        return false;
+    },
+    add: function () {
+        var temp = new THREE.Mesh(this.geometry, this.material);
+        var last = this.body[this.body.length-1];
+        temp.position.set(last.position);
+        this.body.push(temp);
+        this.scene.add(temp);
+    },
+    isSelfCrash: function () {
+        var count = 0;
+        var head = this.body[0];
+        this.body.forEach(function (part) {
+          if(part.position == head.position)
+              count++;
+        });
+        console.log(count);
+        if(count>1)
+            return true;
+        else
+            return false;
     }
 }
 
