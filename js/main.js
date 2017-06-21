c31b3113f5787068fe1673018794acfa6df31469
Yash Kothari <yashkothari1729@gmail.com>
2017-06-21 16:24:20 +0530

    OrbitControl and Keyboard Added

--- js/main.js
+++ js/main.js
@@ -10,18 +10,26 @@ if (Detector.webgl) {
     document.getElementById('container').appendChild(warning);
 }
 */
-
 // Basic Setup
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
-camera.position.set( 0 , -100,50 );
+camera.position.set( 0 , 0,200 );
 camera.lookAt(new THREE.Vector3(0, 0, 0));
-
 var renderer = new THREE.WebGLRenderer({alpha:true});
+renderer.autoClear = false;
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setClearColor( 0xffffff, 0);
 document.body.appendChild( renderer.domElement );
+var geometry1 = new THREE.CubeGeometry(200, 200, 200);
+var material1 = new THREE.MeshBasicMaterial({
+    color: 0xff0000,
+    wireframe: true
+});
 
+mesh = new THREE.Mesh(geometry1, material1);
+scene.add(mesh);
+// Keyboard controls
+var keyboard = new THREEx.KeyboardState();
 function createWall()
 {
     var geometryWall = new THREE.BoxGeometry( 10, 220, 50 );
@@ -41,32 +49,64 @@ function createWall()
     scene.add( cube3 );
     scene.add( cube4 );
 }
-
+//OrbitControls
+controls = new THREE.OrbitControls( camera, renderer.domElement );
+controls.addEventListener( 'change', render ); // remove when using animation loop
+// enable animation loop when using damping or autorotation
+//controls.enableDamping = true;
+//controls.dampingFactor = 0.25;
+controls.enableZoom = false;
 createWall();
 var size = 200;
 var divisions = 20;
 function createGrid(scene){
     var grid = new THREE.GridHelper( size, divisions, 0x000fff,0x234876 );
     grid.position.set(0,0,0);
-    grid.rotation.x=Math.PI/2 ;
-
     scene.add( grid );
 }
-createGrid(scene);
-//var axisHelper = new THREE.AxisHelper( 500 );
-//scene.add( axisHelper );
-renderer.render(scene, camera);
-function render() {
-	requestAnimationFrame( render );
-	renderer.render( scene, camera );
-}
 var snake = new Snake(scene);
-snake.init();
-snake.render();
-setInterval(function(){
-    var k = 10 ;
-    while(k--)
-        snake.move();
 
-},100);
-render();
+createGrid(scene);
+var axisHelper = new THREE.AxisHelper( 500 );
+scene.add( axisHelper );
+renderer.render(scene,camera);
+window.addEventListener( 'resize', onWindowResize, false );
+renderer.clearDepth(); // important! clear the depth buffer
+function keyboardInput() {
+    var change =false;
+    if(keyboard.pressed("w"))
+        if(snake.direction != 3 || snake.direction != 1 )
+        {  snake.direction = 1;change=true; }
+    if(keyboard.pressed("d"))
+        if(snake.direction != 4|| snake.direction != 2 )
+        {   snake.direction = 2;change = true ;}
+    if(keyboard.pressed("s"))
+        if(snake.direction != 1|| snake.direction != 3 )
+        {   snake.direction = 3;change = true;}
+    if(keyboard.pressed("a"))
+        if(snake.direction != 2|| snake.direction != 4 )
+        {   snake.direction = 4;change = true;}
+    if(keyboard.pressed("up"))
+        if(snake.direction != 6|| snake.direction != 6 )
+        {   snake.direction = 5;change = true;}
+    if(keyboard.pressed("down"))
+        if(snake.direction != 5|| snake.direction != 6 )
+        {   snake.direction =6;change = true;}
+   // return change;
+}
+function render(){
+
+        keyboardInput();
+        snake.move();
+	    renderer.render( scene, camera );
+}
+snake.init();
+var fps = 7;
+setInterval( render, 1000/fps  );
+
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
--- js/snake.js
+++ js/snake.js
@@ -1,58 +1,65 @@
 function Snake(scene){
     this.body = [];
-    this.length = 10;
+    this.length = 2;
     this.scene = scene;
-    this.direction = 1;
-    this.geometry = new THREE.BoxGeometry(10,2,10);
+    this.direction = 5 ;
+    this.geometry = new THREE.BoxGeometry(10,10,10);
     this.material = new THREE.MeshLambertMaterial( { color: 0x00ff25 } );
 }
 Snake.prototype = {
-    init : function(){
-        var x = 10*Math.floor(Math.random()*18-9);
-        var y = 10*Math.floor(Math.random()*16-8);
-        for(var i=0;i<10;i++)
-        {
-            var temp = new THREE.Mesh(this.geometry,this.material);
-            temp.position.set(x+5,1+y,5);
-            y+=2;
+    init: function () {
+        var x = 10 * Math.floor(Math.random() * 18 - 9);
+        var y = -10 * Math.floor(Math.random() * 6);
+        var z = 10 * Math.floor(Math.random() * 18 - 9);
+
+        for (var i = 0; i < 2; i++) {
+            var temp = new THREE.Mesh(this.geometry, this.material);
+            temp.position.set(x + 5,  y+5, z+5);
+            y -= 10;
             this.body.push(temp);
         }
-    },
-    move : function(){
-        var temp = this.body.pop();
-      switch (this.direction){
-          case 1 : temp.position.y +=2;break;
-          case 2 : temp.position.x +=2;break;
-          case 3 : temp.position.y -=2;break;
-          case 4 : temp.position.x -=2;break;
-      }
-      this.body.unshift(temp);
-      render();
-    },
-    render : function(){
-        this.body.forEach(function(part){
+        this.body.forEach(function (part) {
             this.scene.add(part);
         })
     },
-    changeCorner: function () {
-        var temp = [];
-        for(var i=0;i<5;i++)
-            temp[i] = this.body.shift();
-        switch (this.direction){
-            case 1 :var x = temp[2].position.x,y = temp[2].position.y-4;
-                    for(var j =0;j<4;j++)
-                    {
-                        temp[i].rotation.z= Math.PI/2;
-                        temp[i].position.set(x,y,5);
-                        y+=2;
-                    }
-                    break;
-            case 2 :temp.position.x +=2;break;
-            case 3 : temp.position.y -=2;break;
-            case 4 : temp.position.x -=2;break;
+    move: function () {
+        var temp = this.body.pop();
+        var firstpos = this.body[0].position;
+        switch (this.direction) {
+            case 1 :
+                temp.position.x = firstpos.x;
+                temp.position.y = firstpos.y;
+                temp.position.z = firstpos.z - 10;
+                break;
+            case 2 :
+                temp.position.x = firstpos.x + 10;
+                temp.position.y = firstpos.y;
+                temp.position.z = firstpos.z;
+                break;
+            case 3 :
+                temp.position.x = firstpos.x;
+                temp.position.y = firstpos.y;
+                temp.position.z = firstpos.z + 10;
+                break;
+            case 4 :
+                temp.position.x = firstpos.x - 10;
+                temp.position.y = firstpos.y;
+                temp.position.z = firstpos.z;
+                break;
+            case 5 :
+                temp.position.x = firstpos.x;
+                temp.position.y = firstpos.y + 10;
+                temp.position.z = firstpos.z;
+                break;
+            case 6 :
+                temp.position.x = firstpos.x;
+                temp.position.y = firstpos.y - 10;
+                temp.position.z = firstpos.z;
+                break;
         }
-        for(var j =4;j>=0;j--)
-            this.body.unshift(temp[j]);
+        this.body.unshift(temp);
     }
-
 }
+
+
+
